86a83f42769e36a18b9c9d14f3984b7f10a59539
yinklylab <yinklylab@gmail.com>
2025-10-08 22:06:43 +0100

    chore(deps): Install file system, Git, and API dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenModule } from './token/token.module';
+import { GithubModule } from './github/github.module';
 
 @Module({
-  imports: [TokenModule],
+  imports: [TokenModule, GithubModule],
   controllers: [AppController],
   providers: [AppService],
 })
